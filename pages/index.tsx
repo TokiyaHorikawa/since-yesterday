888c800b86a0c4d8c0bd800b4860a9ac0027763b
TokiyaHorikawa <ayikot4423@gmail.com>
2020-08-24 01:06:02 +0900

    feat: add float fab button

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,42 +6,54 @@ import {
   ApolloClient,
   InMemoryCache,
 } from "@apollo/client";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import {
+  makeStyles,
+  Theme,
+  createStyles,
+} from "@material-ui/core/styles";
 
  const client = new ApolloClient({
    uri: "http://localhost:8080/v1/graphql",
    cache: new InMemoryCache(),
  });
 
-
+ const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+     fab: {
+       position: 'absolute',
+       bottom: theme.spacing(5),
+       right: theme.spacing(2)
+     }
+   })
+ )
 
 export default function Home() {
+  const classes = useStyles()
   return (
     <ApolloProvider client={client}>
       <div className={styles.container}>
         <Head>
-          <title>Create Next App</title>
+          <title>今日は『昨日より』</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
         <main className={styles.main}>
           <h1 className={styles.title}>
-           今日は『<span>昨日より</span>』
+            今日は『<span>昨日より</span>』
           </h1>
-
-            <ArticleCards></ArticleCards>
-        </main>
-
-        <footer className={styles.footer}>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+          <ArticleCards></ArticleCards>
+          <Fab
+            className={classes.fab}
+            size="medium"
+            aria-label="add"
+            color="primary"
           >
-            Powered by{' '}
-            <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-          </a>
-        </footer>
+            <AddIcon></AddIcon>
+          </Fab>
+        </main>
       </div>
     </ApolloProvider>
-  )
+  );
 }
