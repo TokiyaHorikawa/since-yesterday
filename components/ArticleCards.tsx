d872f4b293410a195b18003854603745a5e93a7d
TokiyaHorikawa <ayikot4423@gmail.com>
2020-09-16 23:31:22 +0900

    feat: カードのstyleをcomponent内に書く

--- components/ArticleCards.tsx
+++ components/ArticleCards.tsx
@@ -1,10 +1,10 @@
 import { useSubscription } from '@apollo/react-hooks'
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import articleStyle from '../styles/ArticleCards.module.css';
 
 import { ARTICLES_SUBSCRIPTIONS } from '../gql/articles';
 
@@ -13,23 +13,40 @@ interface ArticleType {
   text: string;
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    article: {
+      marginTop: "50px",
+    },
+    card: {
+      marginBottom: "5px",
+    },
+    cardActionsBtn: {
+      marginLeft: "auto",
+    },
+  })
+);
+
 const Article = (article: ArticleType) => {
+  const classes = useStyles();
   return (
-    <Card variant="outlined" className={articleStyle.card}>
+    <Card variant="outlined" className={classes.card}>
       <CardContent>
         <Typography variant="body2" component="p">
           {article.text}
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">MORE</Button>
+        <Button size="small" className={classes.cardActionsBtn}>
+          Edit
+        </Button>
       </CardActions>
     </Card>
   );
 }
 
 const ArticleCards = () => {
-
+  const classes = useStyles();
   const { loading, error, data} = useSubscription(ARTICLES_SUBSCRIPTIONS);
   console.log(data)
   // ローディング中の表示
@@ -38,7 +55,7 @@ const ArticleCards = () => {
   if (error) return <p>{error.toString()}</p>
   //　成功してデータが帰ってきた時の表示
   return (
-    <div className={articleStyle.article}>
+    <div className={classes.article}>
       {data.articles.map((article: ArticleType) => (
         <Article {...article} key={article.id}></Article>
       ))}
